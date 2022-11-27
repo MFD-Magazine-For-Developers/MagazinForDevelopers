668136a225e20727db8a7591102f37a14de90255
hanni66 <haeunkim0807@gmail.com>
2022-11-27 16:44:39 +0900

    Feat : 이미지 수정

--- src/js_main/JsContentPanelInfo.java
+++ src/js_main/JsContentPanelInfo.java
@@ -15,13 +15,13 @@ public class JsContentPanelInfo extends ContentPanel {
 		rf.writeTitle("Java Script 소개", this); // 각 챕터의 이름입니다.
 
 		rf.readText("TxtFiles/JSTxt/intro.txt", this);
-		rf.readImage("imges/intro_html.png", defaultImgWidth, 350, this);
+		rf.readImage("images/intro_html.png", defaultImgWidth, 350, this);
 		rf.readText("TxtFiles/JSTxt/css.txt", this);
-		rf.readImage("imges/intro_css.png", defaultImgWidth, 300, this);
+		rf.readImage("images/intro_css.png", defaultImgWidth, 300, this);
 		rf.readText("TxtFiles/JSTxt/js.txt", this);
-		rf.readImage("imges/intro_login.png", defaultImgWidth, 200, this);
+		rf.readImage("images/intro_login.png", defaultImgWidth, 200, this);
 		rf.readText("TxtFiles/JSTxt/js_logIn.txt", this);
-		rf.readImage("imges/intro_js.png", defaultImgWidth, 400, this);
+		rf.readImage("images/intro_js.png", defaultImgWidth, 400, this);
 
 	}
 
--- src/js_main/JsContentPanelUsages.java
+++ src/js_main/JsContentPanelUsages.java
@@ -13,12 +13,12 @@ public class JsContentPanelUsages extends ContentPanel {
 	
 	public JsContentPanelUsages() {
 		rf.writeTitle("JS로 할 수 있는 것들", this);
-		rf.readImage("imges/meta.png", defaultImgWidth, 300, this);
+		rf.readImage("images/meta.png", defaultImgWidth, 300, this);
 		rf.readText("TxtFiles/JSTxt/js_meta.txt", this);
-		rf.readImage("imges/hangout.png", 310, 300, this);
-		rf.readImage("imges/discord.png", 310, 300, this);
+		rf.readImage("images/hangout.png", 310, 300, this);
+		rf.readImage("images/discord.png", 310, 300, this);
 		rf.readText("TxtFiles/JSTxt/hangout.txt", this);
-		rf.readImage("imges/map.png", defaultImgWidth, 300, this);
+		rf.readImage("images/map.png", defaultImgWidth, 300, this);
 		rf.readText("TxtFiles/JSTxt/map.txt", this);
 		
 	}
